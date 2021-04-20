7e68b6c815d96379f6fa0bd1ab89a7fcf4469338
Felipe Sanches <felipebsr97@gmail.com>
2021-04-20 02:16:48 -0300

    app deploy final

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 
 # importar as bibliotecas necessárias
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 import streamlit as st
 from datetime import datetime
@@ -25,23 +26,45 @@ st.markdown(
     """
     Este Dashboard interativo reflete a Análise de Dados feita para o Processo Seletivo da Anatel.
     Código-fonte disponível no meu [Portfólio no Github](https://github.com/felipe-sanches/data_science)
+
+    -
     """
 )
 
+
+st.header('Proporção de Fiscais em análise de atv. centralizada')
+
+lotacao = df['Unidade de Lotação'].unique()
+
 #Filtrando os ficais
-#fiscais = df.loc[df['Unidade de Lotação'] != 'FIGF']
-
-#Filtrando o tipo de Registro
-#fisc_ou_unid = st.selectbox(Filtrar por)
-
-#lotacao = df['Unidade de Lotação'].unique()
-
-
-#option = st.selectbox('Escolha a Unidade de Lotação:',(lotacao))
+fiscais = df.loc[df['Unidade de Lotação'] != 'FIGF']
 
 #filtrando entre os registros total dos fiscais, quais registros são de análises
-#analise = fiscais.loc[fiscais['Tipo de Ação'] == 'Análise']
+analise = fiscais.loc[fiscais['Tipo de Ação'] == 'Análise']
 
+
+#definindo quais sao as atividades centralizadas
+centr = analise[analise['Atividades'].str.contains("SERV")]
+centr = centr[centr['Atividades'].str.contains("entr")]
+centr1= centr[centr['Atividades'].str.contains("SERV_08")]
+
+result = centr.loc[centr.index.difference(centr1.index)]
+
+total = fiscais['Usuários'].unique()
+atvcentr = result['Usuários'].unique()
+
+# Grafico de Pizza apresentando a proporção
+labels = 'Total de fiscais', 'Fiscais atv centralizada'
+sizes = [len(total), len(atvcentr)]
+explode = (0, 0.1)  
+
+fig, ax1 = plt.subplots(figsize = (10,6))
+ax1.pie(sizes, explode=explode, labels=labels, autopct='%1.1f%%',shadow=True, startangle=90)
+ax1.axis('equal')
+st.pyplot(fig)
+
+st.write('-')
+st.header('Usuários com maior Pontuação')
 
 #Definindo input de data inicial e final 
 data_inicio = st.date_input('Selecione a Data Inicial', value = df['Data Entrega'].min() ,min_value= df['Data Entrega'].min(), max_value=df['Data Entrega'].max())
@@ -62,38 +85,63 @@ st.write("Ranking Pontuação de Usuários de ", data_inicio,"a", data_final)
 #Plotando gráfico
 fig, ax = plt.subplots(figsize=(10,6))
 
-score.plot(x="Usuários", kind="barh", ax=ax)
+score.plot(x="Usuários", y= "Pontuação", kind="barh", ax=ax)
 
 ax.set_xlabel("Pontuação")
 ax.set_ylabel("Usuários")
+ax.legend()
 
 for index, value in enumerate(score):
     plt.text(value, index, str(value))
 
 st.pyplot(fig)
 
+
 #Encontrando a unidade que mais pontuou
 agrupapont = df['Pontuação'].groupby(df['Unidade de Lotação']).sum()
 ordenapont = agrupapont.sort_values(ascending= False).head(10)
 
 vis3 = df.loc[df['Unidade de Lotação'] == ordenapont.index[0]]
+nome = ordenapont.index[0]
+
+st.write('-')
+st.header('Unidade com maior Pontuação em todo o período: ' +  nome)
+
+lotacao = np.append(lotacao,'-')
+
+#st.write(lotacao[1].type)
+
+option = st.selectbox('Escolha a Unidade de Lotação que deseja comparar:',(lotacao), index = (len(lotacao)-1))
+
+df2 = df.loc[df['Unidade de Lotação'] == option]
+df2['Data Entrega'] = df2['Data Entrega'].dt.strftime('%Y-%m')
 
 
 #tratando a variavel 'Data Entrega'
 vis3['Data Entrega'] = vis3['Data Entrega'].dt.strftime('%Y-%m')
 
+
+#mudando nome da coluna que sera exibido na legenda
+vis3 = vis3.rename(columns={'Atividades': nome})
+df2 = df2.rename(columns={'Atividades': option})
+
+
 #Agrupando contagem de atividades por mes
-atividade = vis3['Atividades'].groupby(vis3['Data Entrega']).count()
+maiorpont = vis3[nome].groupby(vis3['Data Entrega']).count()
+comparado = df2[option].groupby(df2['Data Entrega']).count()
 
 
 # plotando gráfico de analise 
 fig1, ax = plt.subplots(nrows=1, ncols=1, figsize=(10,4))
 
-atividade.plot(kind="line",ax=ax)
+maiorpont.plot(kind="line",ax=ax)
+comparado.plot(kind="line",ax=ax)
 
+ax.legend()
 ax.set_xlabel("Mês")
 ax.set_ylabel("Quant. Atividades")
-ax.set_title("Atividades por mês da Unidade com Maior Pontuação")
-plt.xticks(range(0,len(atividade.index)), atividade.index, rotation = 45)
+ax.set_title("Atividades por mês")
+plt.xticks(range(0,len(maiorpont.index)), maiorpont.index, rotation = 45)
+
 
 st.pyplot(fig1)
