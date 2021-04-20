062f8d0621533db1347f6a387df9063a70b5b38e
Felipe Sanches <felipebsr97@gmail.com>
2021-04-19 23:49:19 -0300

    app deploy

---
+++ app.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python3
+
+# importar as bibliotecas necessárias
+import pandas as pd
+import matplotlib.pyplot as plt
+import streamlit as st
+from datetime import datetime
+
+#importando base de dados para o dataframe
+df = pd.read_csv('https://raw.githubusercontent.com/felipe-sanches/desafio-anatel/main/Base%20de%20dados%20final.xlsx%20-%20Sheet1.csv')
+
+
+#Convertendo a coluna Data Entrega para o tipo datetime
+df['Data Entrega'] = pd.to_datetime(df['Data Entrega'])
+
+#Trocando virgulas por pontos e convertendo a coluna Pontuação para o tipo Float 
+df['Pontuação'] = df['Pontuação'].str.replace(',', '.')
+df['Pontuação'] = df['Pontuação'].astype(float)
+
+
+#Título e descrição em Texto
+st.title('Desafio - Processo Seletivo ANATEL')
+
+st.markdown(
+    """
+    Este Dashboard interativo reflete a Análise de Dados feita para o Processo Seletivo da Anatel.
+    Código-fonte disponível no meu [Portfólio no Github](https://github.com/felipe-sanches/data_science)
+    """
+)
+
+#Filtrando os ficais
+#fiscais = df.loc[df['Unidade de Lotação'] != 'FIGF']
+
+#Filtrando o tipo de Registro
+#fisc_ou_unid = st.selectbox(Filtrar por)
+
+#lotacao = df['Unidade de Lotação'].unique()
+
+
+#option = st.selectbox('Escolha a Unidade de Lotação:',(lotacao))
+
+#filtrando entre os registros total dos fiscais, quais registros são de análises
+#analise = fiscais.loc[fiscais['Tipo de Ação'] == 'Análise']
+
+
+#Definindo input de data inicial e final 
+data_inicio = st.date_input('Selecione a Data Inicial', value = df['Data Entrega'].min() ,min_value= df['Data Entrega'].min(), max_value=df['Data Entrega'].max())
+data_final = st.date_input('Selecione a Data Final', value = df['Data Entrega'].max() ,min_value= df['Data Entrega'].min(), max_value=df['Data Entrega'].max())
+
+if data_final > data_inicio:
+    periodo = df.loc[(df['Data Entrega'] > pd.to_datetime(data_inicio)) & (df['Data Entrega'] <= pd.to_datetime(data_final))]
+else:
+    st.error('Selecione uma data Final maior do que a Inicial')
+
+#agrupando os usuarios segundo o periodo definido pelo usuario
+soma = periodo['Pontuação'].groupby(periodo['Usuários']).sum()
+score = soma.sort_values(ascending= True).tail(10)
+
+st.write("Ranking Pontuação de Usuários de ", data_inicio,"a", data_final)
+
+
+#Plotando gráfico
+fig, ax = plt.subplots(figsize=(10,6))
+
+score.plot(x="Usuários", kind="barh", ax=ax)
+
+ax.set_xlabel("Pontuação")
+ax.set_ylabel("Usuários")
+
+for index, value in enumerate(score):
+    plt.text(value, index, str(value))
+
+st.pyplot(fig)
+
+#Encontrando a unidade que mais pontuou
+agrupapont = df['Pontuação'].groupby(df['Unidade de Lotação']).sum()
+ordenapont = agrupapont.sort_values(ascending= False).head(10)
+
+vis3 = df.loc[df['Unidade de Lotação'] == ordenapont.index[0]]
+
+
+#tratando a variavel 'Data Entrega'
+vis3['Data Entrega'] = vis3['Data Entrega'].dt.strftime('%Y-%m')
+
+#Agrupando contagem de atividades por mes
+atividade = vis3['Atividades'].groupby(vis3['Data Entrega']).count()
+
+
+# plotando gráfico de analise 
+fig1, ax = plt.subplots(nrows=1, ncols=1, figsize=(10,4))
+
+atividade.plot(kind="line",ax=ax)
+
+ax.set_xlabel("Mês")
+ax.set_ylabel("Quant. Atividades")
+ax.set_title("Atividades por mês da Unidade com Maior Pontuação")
+plt.xticks(range(0,len(atividade.index)), atividade.index, rotation = 45)
+
+st.pyplot(fig1)
